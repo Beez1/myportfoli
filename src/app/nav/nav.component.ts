daf9ce1c04af9a4036a6f8b2ae38a3129e77b2b6
BeEz <98581184+Beez1@users.noreply.github.com>
2024-11-13 03:06:52 +0000

    Update LinkedIn URL in nav component

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -13,7 +13,7 @@ import { RouterModule } from '@angular/router';
 export class NavComponent {
   socialLinks = [
     { text: 'Email', url: 'mailto:emmabassey50@gmail.com', label: 'Email' },
-    { text: 'LinkedIn', url: 'https://www.linkedin.com/in/emmanuel-bassey-1b33b6265?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app', label: 'LinkedIn' },
+    { text: 'LinkedIn',  url: 'https://www.linkedin.com/in/emmanuel-bassey-1b33b6265?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app', label: 'LinkedIn' },
     { text: 'GitHub', url: 'https://github.com/Beez1', label: 'GitHub' }
   ];
 }
